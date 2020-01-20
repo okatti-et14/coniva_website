bb724386d08a038fe4f376e2c2c872bc388c3df2
Iioka <iioka.takumi.quena@gmail.com>
2020-01-20 23:54:08 +0900

    ログインURL変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to controller: "articles", action: "index"
+      redirect_to root_path
     else
       render "new"
     end
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to controller: "articles", action: "index"
+    redirect_to root_path
   end
 end
