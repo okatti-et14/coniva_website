bb3ae6dd00edabba80acf871f62e4012ce89490f
Iioka <iioka.takumi.quena@gmail.com>
2020-02-26 22:51:16 +0900

    前後記事へのリンク実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
   has_one_attached :image
+
+  def get_previous_article
+    Article.where("id < ?", id).order("id DESC").first
+  end
+
+  def get_next_article
+    Article.where("id > ?", id).order("id ASC").first
+  end
 end
