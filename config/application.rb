cfa88b5624998a7fc538e97b6a8308255af20b5d
飯岡巧巳 <iiokatakumimi@adminnoMacBook-Pro.local>
2020-05-10 00:34:47 +0900

    railsのver上げ

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module IbaConivaHp
     config.load_defaults 6.0
     config.active_storage.variant_processor = :vips
     config.time_zone = 'Tokyo'
-    config.i18n.default_locale = :ja
+    config.i18n.fallbacks = [I18n.default_locale]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
